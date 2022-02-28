4a7f3303ccde98a91ec36e7e30d1e8120792b8b3
MalikKtK <99009682+MalikKtK@users.noreply.github.com>
2022-02-28 23:00:26 +0100

    faf

--- 1. semester/EnigmaPrototype/src/ProtoType.java
+++ 1. semester/EnigmaPrototype/src/ProtoType.java
@@ -1,49 +1,49 @@
+import java.util.Scanner;
 
 public class ProtoType {
-    public int convertCharToInt(char c) {
-        // converts range [' ', A-Å] to range [0-29]
-        if (c == ' ') {
-            return 0;
-        } else if (65 <= c && c <= 91) {
-            return (int) c - 64;
-        } else if (c == 'Æ') {
-            return (int) c - 171;
-        } else if (c == 'Ø') {
-            return (int) c - 188;
-        } else { // (c == 'Å')
-            return (int) c - 168;
+    Scanner in = new Scanner(System.in);
+    char x;
+    int numb;
+        public int charToInt(char c) {
+            if (c == ' ') {
+                return 0;
+            } else if (65 <= c && c <= 91) {
+                return (int) c - 64;
+            } else if (c == 'Æ') {
+                return (int) c - 171;
+            } else if (c == 'Ø') {
+                return (int) c - 188;
+            } else {
+                return (int) c - 168;
+            }
+        }
+
+        public char intToChar(int numbers) {
+            if (numbers == 0) {
+                return ' ';
+            } else if (1 <= numbers && numbers <= 26) {
+                return (char) (numbers + 64);
+            } else if (numbers == 27) {
+                return 'Æ';
+            } else if (numbers == 28) {
+                return 'Ø';
+            } else {
+                return 'Å';
+            }
+        }
+
+         void displayTest() {
+            System.out.println("Enter letter: ");
+            x = in.next().charAt(0);
+            System.out.println("The value of " + x + " is " + charToInt(x));
+             System.out.println("Enter Number");
+             numb = in.nextInt();
+             System.out.println("The value of " + numb + " is " + intToChar(numb));
+        }
+
+        public static void main(String[] args) {
+            new ProtoType().displayTest();
+
         }
     }
 
-    public char convertIntToChar(int charValue) {
-        // converts range [0-29] to range [' ', A-Å]
-        if (charValue == 0) {
-            return ' ';
-        } else if (1 <= charValue && charValue <= 26) {
-            return (char) (charValue + 64);
-        } else if (charValue == 27) {
-            return 'Æ';
-        } else if (charValue == 28) {
-            return 'Ø';
-        } else { // (charValue == 29)
-            return 'Å';
-        }
-    }
-
-    public void display(char c) {
-        System.out.printf("'%c' = '%2d' || '%c'\n", c, convertCharToInt(c), convertIntToChar(convertCharToInt(c)));
-    }
-
-    public static void main(String[] args) {
-        ProtoType e = new ProtoType();
-
-        // print test cases
-        e.display(' ');
-        e.display('A');
-        e.display('Z');
-        e.display('Æ');
-        e.display('Ø');
-        e.display('Å');
-
-    }
-}
