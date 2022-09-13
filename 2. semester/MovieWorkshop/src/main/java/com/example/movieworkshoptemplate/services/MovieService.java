228c44ef05b77fda0607f85cdb938b9e0c841706
MalikKtK <mali0237@stud.kea.dk>
2022-09-13 13:51:22 +0200

    fagagagahahdahsh

--- 2. semester/MovieWorkshop/src/main/java/com/example/movieworkshoptemplate/services/MovieService.java
+++ 2. semester/MovieWorkshop/src/main/java/com/example/movieworkshoptemplate/services/MovieService.java
@@ -1,5 +1,24 @@
 package com.example.movieworkshoptemplate.services;
 
+import com.example.movieworkshoptemplate.models.Movie;
+import com.example.movieworkshoptemplate.repositories.MoviesRepository;
+
+import java.util.Random;
+
 public class MovieService {
-    //Services called from controllers that returns what the client requests
+    private MoviesRepository moviesRepository;
+    private Random random;
+
+    public MovieService() {
+        this.moviesRepository = new MoviesRepository();
+    }
+
+
+    public Movie getFirst() {
+        return moviesRepository.getOne(1);
+    }
+
+    public Movie getRandom() {
+        return moviesRepository.getOne(random.nextInt(moviesRepository.getSize()));
+    }
 }
