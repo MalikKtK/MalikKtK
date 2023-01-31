4023e7b4f90b44377d059b1b741166564009eec4
MalikKtK <mali0237@stud.kea.dk>
2023-01-31 13:06:28 +0100

    hey

--- 3. semester/OverloadV1/src/MainInteger.java
+++ 3. semester/OverloadV1/src/MainInteger.java
@@ -1,4 +1,4 @@
-public class App {
+public class MainInteger {
 
     private static void out(String s1) {
         System.out.println(s1);
@@ -11,14 +11,16 @@ public class App {
     private static void out(Integer i2) {
         System.out.println("Jeg er Integer: " + i2);
     }
-    private static void out(boolean b1) {
-        System.out.println("Jeg er boolean: " + b1);
-    }
-    private static void out(double v1) {
-        System.out.println("Jeg er double: " + v1);
-    }
 
     public static void main(String[] args) throws Exception {
-        out(4>5);
-        out(5.4);
+        System.out.println("Hello, World!");
+        out("hej");
+        out(5);
+        var obj = Integer.valueOf(5);
+        out(Integer.valueOf("5"));
+        Integer i1 = 50;
+        out(i1);
+        var obj1 = Integer.parseInt("ff", 16);
+        out(obj1);
+    }
 }
