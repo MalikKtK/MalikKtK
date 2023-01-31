7550faafdaf230ee6b9160c333a7b649f6891ff0
MalikKtK <mali0237@stud.kea.dk>
2023-01-31 13:10:00 +0100

    New update

--- 3. semester/OverloadV1/src/App.java
+++ 3. semester/OverloadV1/src/App.java
@@ -11,14 +11,23 @@ public class App {
     private static void out(Integer i2) {
         System.out.println("Jeg er Integer: " + i2);
     }
+
     private static void out(boolean b1) {
         System.out.println("Jeg er boolean: " + b1);
     }
+
     private static void out(double v1) {
         System.out.println("Jeg er double: " + v1);
     }
 
+    private static void out(Long l1) {
+        System.out.println("Jeg er Long: " + l1);
+    }
+
     public static void main(String[] args) throws Exception {
-        out(4>5);
+        out(4 > 5);
         out(5.4);
+        Long l1 = 456l;
+        out(l1);
+    }
 }
