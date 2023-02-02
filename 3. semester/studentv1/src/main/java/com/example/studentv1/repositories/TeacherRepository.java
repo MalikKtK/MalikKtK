42b31106ed8ed85925e3c9d1466d6c5c6d3455d8
MalikKtK <mali0237@stud.kea.dk>
2023-02-02 12:49:48 +0100

    new

--- 3. semester/studentv1/src/main/java/com/example/studentv1/repositories/TeacherRepository.java
+++ 3. semester/studentv1/src/main/java/com/example/studentv1/repositories/TeacherRepository.java
@@ -2,7 +2,6 @@ package com.example.studentv1.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.studentv1.model.Student;
 import com.example.studentv1.model.Teacher;
 
 public interface TeacherRepository extends JpaRepository<Teacher, Integer> {
